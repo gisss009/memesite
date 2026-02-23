c9aa19d4fe00fa45517da30bc2ad458e47c6d5e3
gisss009 <viktoryagorbaneva@yandex.ru>
2026-02-23 21:52:33 +0300

    add some beauty

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+=begin
+  get "about/contact"
   get "pages/home"
   get "pages/about"
   get "pages/contact"
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
   root "pages#home"   # ← это главная страница сайта
+=end
+  root "pages#home"          # ← это значит: главная страница сайта → PagesController, действие home
+
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
 end
